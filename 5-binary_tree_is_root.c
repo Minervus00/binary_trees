0042b3792bec5d55c760390999b5416b52ed7d20
Minervus00 <fayouedraogo@protonmail.com>
2022-11-15 15:10:29 +0000

    funky

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is a root (doesn't have a parent)
+ * @node: a pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise (and also if node is NULL) 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && node->parent == NULL)
+		return (1);
+	return (0);
+}
