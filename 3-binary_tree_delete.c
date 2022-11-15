9e14b10c6b3afbf98c5d019b5fa70a30e2491adf
Minervus00 <fayouedraogo@protonmail.com>
2022-11-15 11:15:46 +0000

    funky

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_delete - deletes an entire binary tree 
+ * binary_tree_delete - deletes an entire binary tree
  * @tree: a pointer to the root node of the tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
