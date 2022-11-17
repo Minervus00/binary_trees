16abd835887a768e4c5940e9123c24fbd12aa282
Minervus00 <fayouedraogo@protonmail.com>
2022-11-17 16:32:21 +0000

    funky

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -63,11 +63,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t ipow(int b, int e)
 {
 	if (e == 0)
-		return 1;
+		return (1);
 	else if (b == 0)
-		return 0;
+		return (0);
 	else
-		return (b * ipow(b, e-1));
+		return (b * ipow(b, e - 1));
 }
 
 /**
@@ -78,5 +78,6 @@ size_t ipow(int b, int e)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t tmp = ipow(2, (binary_tree_height(tree) + 1));
+
 	return ((tmp - 1) == binary_tree_size(tree));
 }
