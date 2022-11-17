5e2069d9f49d38531065603f87003f28a6dfd8aa
Minervus00 <fayouedraogo@protonmail.com>
2022-11-17 16:42:13 +0000

    funky

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,23 +6,24 @@
  * @second: a pointer to the second node
  * Return: the size or 0 if tree is NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second)
 {
-    binary_tree_t *temp;
+	binary_tree_t *temp;
 
-    if (!first || !second)
-        return (NULL);
-    temp = (binary_tree_t *)second->parent;
-    while (first)
-    {
-        while (second)
-        {
-            if (second == first)
-                return ((binary_tree_t *)first);
-            second = second->parent;
-        }
-        second = temp;
-        first = first->parent;
-    }
-    return (NULL);
+	if (!first || !second)
+		return (NULL);
+	temp = (binary_tree_t *)second->parent;
+	while (first)
+	{
+		while (second)
+		{
+			if (second == first)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = temp;
+		first = first->parent;
+	}
+	return (NULL);
 }
